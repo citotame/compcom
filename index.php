722c7a054986135a80471937c96553e54fc821d4
Rafael <rafael_citotame@hotmail.com>
2015-11-20 04:40:22 -0200

    Inicial

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    
+        <meta charset="UTF-8">
+        <meta name="robots" content="index,follow" >
+        <title>Compcom - Solução em informatica</title>
+        <link rel="icon" href="img/icon/favicon-16.png" sizes="16x16">
+        <link rel="icon" href="img/icon/favicon-32.png" sizes="32x32">
+        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/style2.css" />
+        <link href='https://fonts.googleapis.com/css?family=Amaranth' rel='stylesheet' type='text/css'>
+        <script src="js/javascript.js"></script>
+        <script>
+         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-67189115-1', 'auto');
+          ga('send', 'pageview');
+        </script>
+    
+    
+        <header>
+            <nav id="menu">
+                <a href="index.php"><img src="img/logo.png" /></a>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Quem Somos</a></li>
+                    <li><a href="#">O Que Fazemos</a></li>
+                    <li><a href="#">Encontre-nos</a></li>
+                </ul>
+            </nav>
+        </header>
+        <!--<article>
+            <nav id="navigacao">
+                        <a href="#">
+                            <figure>
+                                <img src="img/plano-casa.jpg" />
+                                <figcaption>Computador lento? Nós podemos te ajudar, clique aqui para saber como.</figcaption>
+                            </figure>
+                        </a>
+                        <a href="#">
+                            <figure>
+                                <img src="img/plano-empresa.jpg" />
+                                <figcaption>Nos Chame para que seus funcionarios fiquem centrados apenos no trabalho, sem dor de cabeça.</figcaption>
+                            </figure>
+                        </a>
+            </nav>
+        </article>-->
+       	        	
+	<span id="sl_play" class="sl_command">&nbsp;</span>
+	<span id="sl_pause" class="sl_command">&nbsp;</span>
+        <span id="sl_i1" class="sl_command sl_i">&nbsp;</span>
+	<span id="sl_i2" class="sl_command sl_i">&nbsp;</span>
+	<span id="sl_i3" class="sl_command sl_i">&nbsp;</span>
+	<span id="sl_i4" class="sl_command sl_i">&nbsp;</span>
+
+	<section id="slideshow">
+	
+		<a class="commands prev commands1" href="#sl_i4" title="Anterior">&lt;</a>
+		<a class="commands next commands1" href="#sl_i2" title="Próximo">&gt;</a>
+		<a class="commands prev commands2" href="#sl_i1" title="Anterior">&lt;</a>
+		<a class="commands next commands2" href="#sl_i3" title="Próximo">&gt;</a>
+		<a class="commands prev commands3" href="#sl_i2" title="Anterior">&lt;</a>
+		<a class="commands next commands3" href="#sl_i4" title="Próximo">&gt;</a>
+		<a class="commands prev commands4" href="#sl_i3" title="Anterior">&lt;</a>
+		<a class="commands next commands4" href="#sl_i1" title="Próximo">&gt;</a>
+		
+		<a class="play_commands pause" href="#sl_pause" title="Parar Animação">Pause</a>
+		<a class="play_commands play" href="#sl_play" title="Iniciar Animação">Play</a>
+		
+		<div class="container">
+			<div class="c_slider"></div>
+                        <div class="slider">
+                            <a href="#"><figure>
+                                    <img src="css/img/plano-casa.jpg" alt="" width="640" height="310" />
+					<figcaption>Computador lento? Nós podemos te ajudar, clique aqui para saber como.</figcaption>
+                                </figure></a><!--
+				--><a href="#"><figure>
+                                    <img src="css/img/plano-empresa.jpg" alt="" width="640" height="310" />
+					<figcaption>Para que dor de cabeça? A Compcom resolve seus problemas.</figcaption>
+                                    </figure></a><!--
+				--><a href="#"><figure>
+                                    <img src="css/img/plano-casa.jpg" alt="" width="640" height="310" />
+					<figcaption>Computador lento? Nós podemos te ajudar, clique aqui para saber como.</figcaption>
+                                    </figure></a><!--
+				--><a href="#"><figure>
+                                    <img src="css/img/plano-empresa.jpg" alt="" width="640" height="310" />
+					<figcaption>Para que dor de cabeça? A Compcom resolve seus problemas.</figcaption>
+                                    </figure></a>
+                            </div>
+		</div>
+		
+		<span id="timeline"></span>
+		
+		<ul class="dots_commands"><!--
+			--><li><a title="Show slide 1" href="#sl_i1">Slide 1</a></li><!--
+			--><li><a title="Show slide 2" href="#sl_i2">Slide 2</a></li><!--
+			--><li><a title="Show slide 3" href="#sl_i3">Slide 3</a></li><!--
+			--><li><a title="Show slide 4" href="#sl_i4">Slide 4</a></li>
+		</ul>
+	</section>
+        <footer>
+            <ul id="rodape">
+                <li><a href="#">teste</a></li>
+                <li><a href="#">teste</a></li>
+                <li><a href="#">teste</a></li>
+                <li><a href="#">teste</a></li>
+            </ul>
+        </footer>
+    
+</html>
+
+
---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+    @font-face {
+        font-family: danube;
+        src: url(fonts/DANUBE.TTF)format("truetype");
+    
+
+    background: -webkit-linear-gradient(#FFF, #FFF 20%, #EEE 80%, #DDD);
+    background: -moz-linear-gradient(#FFF, #FFF 20%, #EEE 80%, #DDD);
+    background: -ms-linear-gradient(#FFF, #FFF 20%, #EEE 80%, #DDD);
+    background: -o-linear-gradient(#FFF, #FFF 20%, #EEE 80%, #DDD);
+        -webkit-border-radius: 2px 2px 2px 2px;
+    -moz-border-radius: 2px 2px 2px 2px;
+        -webkit-box-shadow: 0 0 10px rgba(0,0,0, 0.2);
+    -moz-box-shadow: 0 0 10px rgba(0,0,0, 0.2);
